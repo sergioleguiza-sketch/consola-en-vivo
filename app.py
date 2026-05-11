ac700abb11199e0d9e6624ee9d29268ba165865e
sergioleguiza-sketch <sergio.leguiza@gmail.com>
2026-05-11 19:58:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -147,10 +147,10 @@ def registrar_suceso_inteligente(id_evento, dorsal, patio_sistema, hora_cero_db,
     try:
         supabase.table("vueltas_vivo").insert(nuevo_registro).execute()
         emoji = "✅" if estado == "ACT" else "⚠️"
-        return f"{emoji} Bib {dorsal}: {estado} en Patio {nro_vuelta}"
+        return f"{emoji} Bib {dorsal}: {estado} en Patio {nro_vuelta_registro}"
     except Exception as e:
         return f"❌ Error real: {str(e)}"
-        #return f"❌ Error: El dorsal {dorsal} ya tiene registro en el Patio {nro_vuelta}."
+        #return f"❌ Error: El dorsal {dorsal} ya tiene registro en el Patio {nro_vuelta_registro}."
 
 def obtener_estado_monitor(id_evento, nro_vuelta):
     # 1. Traemos inscripciones: asistente está aquí, y anidamos atletas para el nombre
